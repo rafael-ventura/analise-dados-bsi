b7fa366cd194a72011ddcd8bcf9a91144930aef0
rafa <raf4ael@gmail.com>
2022-06-05 00:40:37 -0300

    /a

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from turtle import color
 import pandas as pd
 import plotly.express as px
 
@@ -13,22 +12,26 @@ server = app.server
 #pandas lendo nosso CSV
 df = pd.read_csv('dfEvasao.csv')
 dfPura = pd.read_csv('dfFormaEvPura.csv')
+dfCotasPie = pd.read_csv('dfCotasPie.csv')
 
 fig = px.bar(df, x='ANO_EVASAO',y='ALUNOS',color='FORMA_EVASAO', barmode='group')
-fig2 =go.Figure(go.Sunburst(
-    labels=['Evasão','ABA - Abandono do curso',
-            'APO - Aluno Especial - Disciplina Isolada', 'APO -Conclusão a Mobilidade Acadêmica Externa - ANDIFES',
-            'APO -Conclusão da Mobilidade Acadêmica Externa - IAE/IESCE	','CAN - Cancelamento Geral do curso',
-            'CON - Curso concluído','Desistência SiSU', 'JUB - Jubilamento', 'Não identificada (vide pasta do aluno)' ,
-            'Sem evasão'
-    ],
-    parents=["", "Evasão", "Evasão", "Evasão", "Evasão", "Evasão", "",
-            "Evasão", "Evasão", "Evasão", "" ],
-    values=[507,186,3,3,2,173,335,15,120,5,418]
-))
-fig2.update_layout(margin = dict(t=0, l=0, r=0, b=0))
-
-
+fig4 = px.pie(dfCotasPie, values=dfCotasPie['ALUNOS'],
+                names= dfCotasPie['FORMA_INGRESSO'] ,
+                title='Quantidade de aluno por forma de ingresso',
+                color=dfCotasPie['FORMA_INGRESSO'],
+                labels={
+                    'FORMA_INGRESSO' : 'Modalidade',
+                    'value' : 'Alunos'
+                }
+        )
+fig4.update_traces(hoverinfo='label', textinfo='percent', textfont_size=20)
+fig4.update_layout( 
+        font=dict(
+        family="Courier New, monospace",
+        size=13,
+        color="RebeccaPurple"
+    ))  ,
+                                            
 fig3 = px.pie(  dfPura, values=dfPura['ALUNOS'], 
                 names=dfPura['FORMA_EVASAO'], 
                 title='Situacao dos Alunos de Sistema de Informacao',
@@ -67,6 +70,32 @@ app.layout = html.Div(children=[
             dcc.Graph(
                 id='my_bsi_pizza_graph',
                 figure=fig3,
+                style={
+                    "width": "100%",
+                    "height": "100%"
+                }
+            ),
+            style={
+                "width": "100%",
+                "height": "100%",
+            },
+        ),
+        style={
+                "width": "85%",
+                "height": "800px",
+                "display": "inline-block",
+                "padding-top": "5px",
+                "padding-left": "1px",
+                "overflow": "hidden"
+            }
+    ),
+
+    html.Br(),
+    html.Div(
+        html.Div(
+            dcc.Graph(
+                id='cotasPie',
+                figure=fig4,
                 style={
                     "width": "100%",
                     "height": "100%"
